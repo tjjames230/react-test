a877d127e90be8527b09d150637ebf4ae8e164c6
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-10-11 14:18:31 -0500

    added some notes from react docs for future reference

--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,22 @@
-const List = () => {
-	const animals = ["dog", "frog", "cow", "chicken", "sloth"];
+const ListItem = (props) => {
+	return <li>{props.animal}</li>;
+};
+
+const List = (props) => {
+	if (!props.animals) {
+		return <div>Loading...</div>;
+	}
+
+	if (props.animals.length === 0) {
+		return <div>There are no animals!</div>;
+	}
 
 	return (
-		<>
-			<h1>List:</h1>
-			<ul>
-				{animals.map((animal) => {
-					return <li key={animal}>{animal}</li>;
-				})}
-			</ul>
-		</>
+		<ul>
+			{props.animals.map((animal) => {
+				return <ListItem key={animal} animal={animal} />;
+			})}
+		</ul>
 	);
 };
 
